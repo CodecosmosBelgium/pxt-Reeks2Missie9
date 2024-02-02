f4ee16a5321a8e0ea4534f655c2c1783e4c5f69f
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2024-02-02 14:03:56 +0100

    custom_functions.ts bijwerken

--- custom_functions.ts
+++ custom_functions.ts
@@ -3,18 +3,101 @@ function testForTNT(): boolean {
     return blocks.testForBlock(TNT, posBelowAgent)
 }
 
+function testBlock(block: number): boolean {
+    let posAgent = agent.getPosition()
+    return blocks.testForBlock(block, posAgent)
+}
+
+function testBlockUnder(block: number): boolean {
+    let posBelowAgent = agent.getPosition().add(world(0, -1, 0))
+    return blocks.testForBlock(block, posBelowAgent)
+}
+
+function testBlockNextTo(block: number): boolean {
+    return (
+        blocks.testForBlock(block, agent.getPosition().add(pos(0, -1, 1))) || 
+        blocks.testForBlock(block, agent.getPosition().add(pos(0, -1, -1))) || 
+        blocks.testForBlock(block, agent.getPosition().add(pos(1, -1, 0))) || 
+        blocks.testForBlock(block, agent.getPosition().add(pos(-1, -1, 0)))
+    )
+}
+
+function ironBarsLeft(): boolean {
+    return agent.inspect(AgentInspection.Block, FORWARD) == IRON_BARS
+}
+
+function torchRight(): boolean {
+    return agent.inspect(AgentInspection.Block, FORWARD) == TORCH
+}
+
+const wrong = () => {
+    player.execute(`execute @c ~ ~ ~ particle rwm:barrier ~ ~1 ~`)
+    player.execute(`scoreboard players add @a correctBlocks -100`)
+}
+
+enum AgentTurn {
+    //% block="left"
+    Left = TurnDirection.Left,
+    //% block="right"
+    Right = TurnDirection.Right
+}
+
+
 //% color="#D83B01" weight=100 icon="\uf20a" block="AgentExtension"
 namespace AgentExtension {
     //% block="agent move $direction by $amount"
-    //% amount.defl=1
-    //% amount.min=1
     export function agentMoveFourDirection(direction: FourDirection, amount: number) {
         for (let i = 0; i < amount; i++) {
-            agent.move(direction, 1)
             player.execute(`scoreboard players set @a level_timer 0`)
+            agent.move(direction, 1)
             loops.pause(100)
             testForTNT() && player.execute(`function exercises/ex_1/tnt`)
         }
+    }
+    
+    //% block="agent next to bamboo"
+    export function agentNextToBamboo(): boolean {
+        return testBlockNextTo(BAMBOO)
+    }
+    
+    //% block="agent on fire"
+    export function agentOnFire(): boolean {
+        return (testBlockUnder(FIRE))
+    }
+
+    //% block="agent next to bush"
+    export function agentNextToBush(): boolean {
+        return testBlockNextTo(FLOWERING_AZALEA_LEAVES)
+    }
+
+    //% block="agent turn $direction"
+    export function agentTurn(direction: AgentTurn) {
+        agent.turn(direction)
+        if (ironBarsLeft() && direction == AgentTurn.Left) {
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else if (torchRight() && direction == AgentTurn.Right) {
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+        player.execute(`scoreboard players set @a level_timer 0`)
+    }
+
+    //% block="agent turn 360 degrees"
+    export function agent360() {
+        if (!(testBlock(IRON_BLOCK) && testBlockNextTo(OAK_SAPLING))
+            && !(testBlockUnder(GRASS) && testBlockNextTo(FLOWERING_AZALEA_LEAVES))
+            && !(ironBarsLeft)
+            && !(torchRight)
+        ) {
+            for (let i = 0; i < 4; i++) {
+                agent.turn(RIGHT_TURN)
+            }
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+        player.execute(`scoreboard players set @a level_timer 0`)
     }
 }
 
@@ -26,5 +109,89 @@ namespace CodeCosmos {
         player.execute(`scoreboard players set @a level_timer 0`)
         player.execute(`function exercises/ex_1/pickup_tool`)
         loops.pause(100);
+    }
+
+    //% block="is block %block=block below agent"
+    //% block.shadow=minecraftBlock
+    export function testForBlock_belowAgent(block: number): boolean {
+        let posBelowAgent = agent.getPosition().add(world(0, -1, 0))
+        player.execute(`scoreboard players set @a level_timer 0`)
+        return blocks.testForBlock(block, posBelowAgent)
+    }
+
+    //% block="place tree"
+    export function placeTree() {
+        if (testBlockUnder(GRASS) || testBlockUnder(DIRT)) {
+            player.execute(`execute @v ~ ~ ~ setblock ~ ~ ~ sapling`)
+            player.execute(`function levels/place/tree`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+    //% block="place bush"
+    export function placeBush() {
+        if (!(testBlockUnder(GRASS) || testBlockUnder(DIRT))) {
+            player.execute(`execute @v ~ ~ ~ setblock ~ ~ ~ flowering_azalea`)
+            player.execute(`function levels/place/bush`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+    
+    //% block="spawn panda"
+    export function spawnPanda() {
+        if (testBlockUnder(GRASS) || testBlockNextTo(BAMBOO)) {
+            player.execute(`execute @v ~ ~1 ~ summon panda`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+    
+    //% block="extinguish fire"
+    export function extinguishFire() {
+        if (testBlockUnder(FIRE)) {
+            player.execute(`execute @v ~ ~ ~ setblock ~ ~ ~ air`)
+            player.execute(`function levels/place/air`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+    //% block="place bush"
+    export function placeBush2() {
+        if ((!(testBlockUnder(GRASS) || testBlockNextTo(BAMBOO))) && !testBlockUnder(FIRE)) {
+            player.execute(`execute @v ~ ~ ~ setblock ~ ~ ~ flowering_azalea`)
+            player.execute(`function levels/place/bush`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+
+    //% block="spawn ocelot"
+    export function spawnOcelot() {
+        if (testBlockUnder(GRASS) && testBlockNextTo(FLOWERING_AZALEA_LEAVES)) {
+            player.execute(`execute @v ~ ~1 ~ summon ocelot`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+    //% block="spawn parrot"
+    export function spawnParrot() {
+        if (testBlock(IRON_BLOCK) && testBlockNextTo(OAK_SAPLING)) {
+            player.execute(`execute @v ~ ~1 ~ summon parrot`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
     }
 }
